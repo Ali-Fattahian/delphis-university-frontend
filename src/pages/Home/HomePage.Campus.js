5e9ca6a60190b9b374c0f5b4bbaf8413e9711b1f
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-11-04 01:20:33 +0330

    create campus section in homepage

--- src/pages/Home/HomePage.Campus.js
+++ src/pages/Home/HomePage.Campus.js
@@ -1,7 +1,47 @@
 import React from "react";
 
+const imageURL = require("../../assets/images/university.jpeg");
+
 const HomePageCampus = () => {
-  return <div>HomePage.Campus</div>;
+  return (
+    <div className="w-full relative">
+      <img
+        className="object-cover w-full"
+        src={imageURL}
+        alt="University Campus"
+      />
+      <div className="h-full w-full backdrop-blur-md absolute top-0 left-0"></div>
+      <div className="w-full h-full p-4 absolute top-10 left-0 text-white text-center">
+        <h1 className="text-4xl font-semibold mb-10">OUR CAMPUS</h1>
+        <div className="w-full flex justify-center">
+          <div className="w-full sm:w-1/2 flex flex-col gap-6 m-[right]">
+            <p className="font-semibold">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+            <div className="p-6 flex flex-col gap-4 justify-center items-center max-w-[500px]">
+              <a
+                href="/"
+                className="px-4 py-3 bg-transparent text-white text-sm font-semibold border-[1px]  transition-colors duration-300 hover:bg-[#ffffff14] min-w-[200px]"
+              >
+                SCHEDULE A TOUR
+              </a>
+              <a
+                href="/"
+                className="px-4 py-3 bg-transparent text-white text-sm font-semibold border-[1px]  transition-colors duration-300 hover:bg-[#ffffff14] min-w-[200px]"
+              >
+                OUR HOMETOWN
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePageCampus;
