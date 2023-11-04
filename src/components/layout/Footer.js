31edab194a4287d45241bb34c55c520ca4274aca
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-11-04 03:49:48 +0330

    create the footer and add it to the homepage

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,296 @@
 import React from "react";
 
+const imageURL = require("../../assets/images/Logo.jpg");
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="border-t-4 border-vgyellow bg-">
+      <div className="grid grid-cols-1 min-[400px]:grid-cols-2 sm:grid-cols-4 gap-4 p-4">
+        <div className="flex flex-col gap-1">
+          <h3 className="text-vgyellow font-semibold">COLLEGES AND SCHOOLS</h3>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Arts and Sciences
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Capstone College of Nursing
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Communication & Information Sciences
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Community Health Sciences
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Culverhouse College of Business
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Education
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Engineering
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Graduate School
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Honors College
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Human Environmental Sciences
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            School of Law
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Social Work
+          </a>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h3 className="text-vgyellow font-semibold">ADMINISTRATION</h3>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            President
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Academic Affairs
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Diversity, Equity and Inclusion
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Community Affairs
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Finance and Operations
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Research & Economic Development
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Strategic Communications
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Student Life
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Advancement
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            The University of Alabama System
+          </a>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h3 className="text-vgyellow font-semibold">RESOURCES</h3>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Campus Map
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Directory
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Distance Education
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Information Technology
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Jobs
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Libraries
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Museums
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Policies
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Quick Facts
+          </a>
+          <a
+            href="/"
+            className="text-xs font-semibold hover:underline cursor-pointer"
+          >
+            Visitors Guide
+          </a>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
+            <h3 className="text-vgyellow font-semibold">SAFETY</h3>
+            <a
+              href="/"
+              className="text-xs font-semibold hover:underline cursor-pointer"
+            >
+              University Police
+            </a>
+            <a
+              href="/"
+              className="text-xs font-semibold hover:underline cursor-pointer"
+            >
+              Crime Advisories
+            </a>
+            <a
+              href="/"
+              className="text-xs font-semibold hover:underline cursor-pointer"
+            >
+              Emergency Management
+            </a>
+            <a
+              href="/"
+              className="text-xs font-semibold hover:underline cursor-pointer"
+            >
+              Reporting (UDct)
+            </a>
+            <a
+              href="/"
+              className="text-xs font-semibold hover:underline cursor-pointer"
+            >
+              Safety & Support
+            </a>
+            <a
+              href="/"
+              className="text-xs font-semibold hover:underline cursor-pointer"
+            >
+              Environmental Health and Safety
+            </a>
+            <a
+              href="/"
+              className="text-xs font-semibold hover:underline cursor-pointer"
+            >
+              Threat Assessment
+            </a>
+            <div className="flex flex-col gap-1">
+              <h3 className="text-vgyellow font-semibold">HELP</h3>
+              <a
+                href="/"
+                className="text-xs font-semibold hover:underline cursor-pointer"
+              >
+                Find Websites
+              </a>
+              <a
+                href="/"
+                className="text-xs font-semibold hover:underline cursor-pointer"
+              >
+                Find People
+              </a>
+              <a
+                href="/"
+                className="text-xs font-semibold hover:underline cursor-pointer"
+              >
+                Search UD.edu
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gradient-to-l from-vgblue to-[#3e617a] flex h-[80px] justify-between">
+        <div className="flex-grow flex justify-center items-center">
+          <p className="text-white text-center p-4 text-sm font-semibold">
+            Copyright © 2023 The University of{" "}
+            <span className="text-vgyellow">Delphis</span>
+          </p>
+        </div>
+        <img
+          src={imageURL}
+          alt={`Website's Logo`}
+          className="h-[80px] min-w-[102px]"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
