426d30b11ff76502eb8e270fc057e8b951633d3b
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-11-20 16:38:13 +0330

    finish the EventsCalender page

---
+++ src/pages/Calender/EventsCalenderPage.Card.js
@@ -0,0 +1,61 @@
+import ComputerDesktopIcon from "@heroicons/react/24/outline/ComputerDesktopIcon";
+import MapPinIcon from "@heroicons/react/24/outline/MapPinIcon";
+import React from "react";
+
+const EventsCalenderPageCard = ({
+  imageURL,
+  title,
+  tags,
+  date,
+  location,
+  isFree,
+  link,
+  isVirtual,
+}) => {
+  return (
+    <div className="flex flex-col gap-1 items-center mx-auto relative max-w-[260px] bg-white pb-3 transition-all duration-300 hover:shadow-md">
+      <img className="object-cover aspect-square" src={imageURL} alt={title} />
+      {tags && (
+        <div className="absolute top-0 left-0 p-2">
+          {tags.map((tag) => (
+            <p key={tag.key} className="bg-vggreen p-1 text-xs text-white">
+              {tag.title}
+            </p>
+          ))}
+        </div>
+      )}
+      <a
+        className="pt-1 px-2 text-vgyellow hover:underline cursor-pointer w-full"
+        href={link}
+      >
+        {title}
+      </a>
+      <p className="pt-2 px-2 text-xs text-slate-800 w-full">{date}</p>
+      {location && (
+        <p className="pt-2 px-2 text-xs text-slate-800 w-full">
+          <MapPinIcon
+            width={16}
+            height={16}
+            className="mr-1 mb-1 inline-block text-vgred"
+          />
+          {location}
+        </p>
+      )}
+      {isVirtual && (
+        <p className="pt-1 px-2 text-xs text-slate-800 w-full">
+          <ComputerDesktopIcon
+            width={16}
+            height={16}
+            className="mr-1 mb-1 inline-block text-vgred"
+          />
+          Virtual Event
+        </p>
+      )}
+      {isFree && (
+        <p className="pt-1 px-2 text-xs text-slate-800 w-full">Free</p>
+      )}
+    </div>
+  );
+};
+
+export default EventsCalenderPageCard;
--- src/pages/Calender/EventsCalenderPage.js
+++ src/pages/Calender/EventsCalenderPage.js
@@ -1,10 +1,175 @@
-import React from "react";
+import React, { useState } from "react";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
+import Navbar from "../../components/layout/Navbar";
+import Footer from "../../components/layout/Footer";
+import EventsCalenderPageCard from "./EventsCalenderPage.Card";
 
 const EventsCalenderPage = () => {
   useDocumentTitle("Events Calender");
+  const [show, setShow] = useState(0);
+  const image = require("../../assets/images/Tour - eating.jpeg");
 
-  return <div>EventsCalenderPage</div>;
+  const events = [
+    {
+      key: 1,
+      title: "Thanksgiving Week - Classes Dismissed",
+      date: "Mon, Nov 20, 2023",
+      tags: [
+        {
+          key: 1,
+          title: "Academic Dates & Deadlines",
+        },
+      ],
+      free: false,
+      virtual: false,
+      imageURL: image,
+      link: "/",
+      location: "Gorgas Library, 2nd Floor , Quad Entrance/Foyer",
+      isVirtual: false,
+    },
+    {
+      key: 2,
+      title: "The Northern Edge of the Gulf of Mexico",
+      date: "Mon, Nov 20, 2023",
+      tags: false,
+      free: false,
+      virtual: false,
+      imageURL: image,
+      link: "/",
+      isVirtual: true,
+    },
+    {
+      key: 3,
+      title: "Thanksgiving Week - Classes Dismissed",
+      date: "Mon, Nov 20, 2023",
+      tags: false,
+      free: true,
+      virtual: false,
+      imageURL: image,
+      link: "/",
+      isVirtual: true,
+    },
+  ];
+
+  return (
+    <>
+      <Navbar />
+      <div className="w-full mt-[36px] min-[720px]:mt-0 bg-neutral-100">
+        <div className="w-full max-w-[1200px] m-auto p-4">
+          <div className="border-b-[1px] border-neutral-300">
+            <h2 className="text-2xl font-semibold p-2">Events</h2>
+          </div>
+          <div className="mt-4 grid grid-cols-2 sm:grid-cols-4 text-center gap-4 max-w-[800px] mx-auto justify-center text-vgblue shadow-sm px-2 py-4">
+            <div className="flex flex-col group">
+              <h3
+                className="text-1xl cursor-pointer overflow-y-hidden"
+                onClick={() => setShow(0)}
+              >
+                By Date
+              </h3>
+              <div className="transition-all duration-300 w-full h-[1px] bg-vgblue scale-0 group-hover:scale-100"></div>
+            </div>
+            <div className="flex flex-col group">
+              <h3
+                className="text-1xl cursor-pointer overflow-y-hidden"
+                onClick={() => setShow(1)}
+              >
+                Trending
+              </h3>
+              <div className="transition-all duration-300 w-full h-[1px] bg-vgblue scale-0 group-hover:scale-100"></div>
+            </div>
+            <div className="flex flex-col group">
+              <h3
+                className="text-1xl cursor-pointer overflow-y-hidden"
+                onClick={() => setShow(2)}
+              >
+                Academic Calender
+              </h3>
+              <div className="transition-all duration-300 w-full h-[1px] bg-vgblue scale-0 group-hover:scale-100"></div>
+            </div>
+            <div className="flex flex-col group">
+              <h3
+                className="text-1xl cursor-pointer overflow-y-hidden"
+                onClick={() => setShow(3)}
+              >
+                Student Events
+              </h3>
+              <div className="transition-all duration-300 w-full h-[1px] bg-vgblue scale-0 group-hover:scale-100"></div>
+            </div>
+          </div>
+          {show === 0 && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center w-full max-w-[800px] mx-auto my-6">
+              {events.map((event) => (
+                <EventsCalenderPageCard
+                  key={event.key}
+                  imageURL={event.imageURL}
+                  title={event.title}
+                  tags={event.tags}
+                  date={event.date}
+                  location={event.location}
+                  isFree={event.free}
+                  link={event.link}
+                  isVirtual={event.isVirtual}
+                />
+              ))}
+            </div>
+          )}
+          {show === 1 && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center w-full max-w-[800px] mx-auto my-6">
+              {events.map((event) => (
+                <EventsCalenderPageCard
+                  key={event.key}
+                  imageURL={event.imageURL}
+                  title={event.title}
+                  tags={event.tags}
+                  date={event.date}
+                  location={event.location}
+                  isFree={event.free}
+                  link={event.link}
+                  isVirtual={event.isVirtual}
+                />
+              ))}
+            </div>
+          )}
+          {show === 2 && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center w-full max-w-[800px] mx-auto my-6">
+              {events.map((event) => (
+                <EventsCalenderPageCard
+                  key={event.key}
+                  imageURL={event.imageURL}
+                  title={event.title}
+                  tags={event.tags}
+                  date={event.date}
+                  location={event.location}
+                  isFree={event.free}
+                  link={event.link}
+                  isVirtual={event.isVirtual}
+                />
+              ))}
+            </div>
+          )}
+          {show === 3 && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center w-full max-w-[800px] mx-auto my-6">
+              {events.map((event) => (
+                <EventsCalenderPageCard
+                  key={event.key}
+                  imageURL={event.imageURL}
+                  title={event.title}
+                  tags={event.tags}
+                  date={event.date}
+                  location={event.location}
+                  isFree={event.free}
+                  link={event.link}
+                  isVirtual={event.isVirtual}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default EventsCalenderPage;
